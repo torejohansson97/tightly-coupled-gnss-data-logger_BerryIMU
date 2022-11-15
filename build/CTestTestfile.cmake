190ec16694126b15210e5897c96e5c1f26c66177
Tore Johansson <torejohansson1997@gmail.com>
2022-11-15 14:05:01 +0100

    Removed build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tore/Documents/F - Utbildning/B - Master/IL2232/tightly-coupled-gnss-data-logger_imu_node
-# Build directory: /home/tore/Documents/F - Utbildning/B - Master/IL2232/tightly-coupled-gnss-data-logger_imu_node/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("RTIMULib")
